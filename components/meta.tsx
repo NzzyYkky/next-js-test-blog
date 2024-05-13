0328e7313cf25231cef0e85e50f66d245120ed85
NzzyYkky <62602398+NzzyYkky@users.noreply.github.com>
2024-05-13 23:00:52 +0900

    fix:metaタグ、indexページをtypescriptへ

---
+++ components/meta.tsx
@@ -0,0 +1,59 @@
+import Head from 'next/head';
+import { siteMeta } from 'lib/constant';
+import { useRouter } from 'next/router';
+import siteImg from 'images/ogp.png';
+
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
+	siteMeta;
+
+type MetaProps = {
+	pageTitle?: string;
+	pageDesc?: string;
+	pageImage?: string;
+	pageImageW?: number;
+	pageImageH?: number;
+};
+
+export default function Meta({
+	pageTitle,
+	pageDesc,
+	pageImage,
+	pageImageW,
+	pageImageH,
+}: MetaProps): JSX.Element {
+	const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
+	const desc = pageDesc ?? siteDesc;
+
+	const router = useRouter();
+	const url = `${siteUrl}${router.asPath}`;
+
+	const img = pageImage || siteImg.src;
+	const imgW = pageImageW || siteImg.width;
+	const imgH = pageImageH || siteImg.height;
+	const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`;
+
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta property="og:title" content={title} />
+
+			<meta name="description" content={desc} />
+			<meta property="og:description" content={desc} />
+
+			<link rel="canonical" href={url} />
+			<meta property="og:url" content={url} />
+
+			<meta property="og:site_name" content={siteTitle} />
+			<meta property="og:type" content={siteType} />
+			<meta property="og:locale" content={siteLocale} />
+
+			<link rel="icon" href={siteIcon} />
+			<link rel="apple-touch-icon" href={siteIcon} />
+
+			<meta property="og:image" content={imgUrl} />
+			<meta property="og:image:width" content={String(imgW)} />
+			<meta property="og:image:height" content={String(imgH)} />
+			<meta name="twitter:card" content="summary_large_image" />
+		</Head>
+	);
+}
