674a6ff8754d41f66de4f66db93a20f132772ede
NzzyYkky <62602398+NzzyYkky@users.noreply.github.com>
2024-05-13 22:49:30 +0900

    update:typescript環境追加

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.microcms-assets.io']
-  }
+  },
+  swcMinify: true,
+  swcLoader: true,
 }
 
 module.exports = nextConfig
